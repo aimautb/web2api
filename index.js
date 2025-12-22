b0eb729ffaae4b65ff8f7048250444093709918f
aimautb <abolathan3@gmail.com>
2025-12-22 14:23:03 +0500

    server test by get /

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({extended : true }));
+app.use(express.json());
 
-
+app.get("/", (req , res )=>{
+    res.send("<h1>SERVER IS RUNNING</h1>")
+}
+);
 
 app.listen(PORT, ()=> {
     console.log(`The server running on the port:${PORT}`)
